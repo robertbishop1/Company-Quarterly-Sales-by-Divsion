c134f8e45a0a6ca53ac399bdda2084e34665ed3b
Robert Bishop <robert.bishop001@mymdc.net>
2017-06-14 13:48:50 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,10 +8,9 @@ using std::cin;
 double getSales() {
     double sales;
     cin >> sales;
-    if(sales < 0) {
+    if(sales < 0);
         cout << "Error, sales must be 0 or larger" << endl;
-        return 0;
-    }
+
         return sales;
 }
 
@@ -19,9 +18,14 @@ int main() {
     double northDiv, southDiv, eastDiv, westDiv, northWestDiv, northEastDiv,
             southWestDvi, southEastDiv;
 
-    cout << "Enter North Divisions total sales for the quarter:";
+    cout << "Enter North's quarterly sales:";
     northDiv = getSales();
-    cout << northDiv;
+    cout << "Enter South's quarterly sales:";
+    southDiv = getSales();
+    cout << "Enter East's quarterly sales:";
+    eastDiv = getSales();
+    cout << "Enter West's quarterly sales:";
+    westDiv = getSales();
 
     return 0;
 }
