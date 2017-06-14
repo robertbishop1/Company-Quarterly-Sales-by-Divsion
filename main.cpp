d0d3a6b3ad6789b0a567ba5807c9d55a524796e7
Robert Bishop <robert.bishop001@mymdc.net>
2017-06-14 13:50:58 -0400

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ using std::cin;
 double getSales() {
     double sales;
     cin >> sales;
+    
+    // The input must be 0 or higher.
     if(sales < 0);
         cout << "Error, sales must be 0 or larger" << endl;
 
@@ -15,9 +17,10 @@ double getSales() {
 }
 
 int main() {
+    // This company has eight divisions listed below.
     double northDiv, southDiv, eastDiv, westDiv, northWestDiv, northEastDiv,
             southWestDvi, southEastDiv;
-
+    // Gathering sales data for each division.
     cout << "Enter North's quarterly sales:";
     northDiv = getSales();
     cout << "Enter South's quarterly sales:";
